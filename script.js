3c19f210a734830e40973e0ced470a9f31e795a3
Isaac Chabon <ichabon@me.com>
2021-12-21 23:53:46 -0500

    more styling updates

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
+function initPayPalButton() {
+  paypal.Buttons({
+    style: {
+      shape: 'rect',
+      color: 'gold',
+      layout: 'vertical',
+      label: 'paypal',
+    },
+    createOrder: function(data, actions) {
+      return actions.order.create({
+        purchase_units: [{"amount":{"currency_code":"USD","value":1}}]
+      });
+    },
+    onApprove: function(data, actions) {
+      return actions.order.capture().then(function(orderData) {
+        console.log('Capture result', orderData, JSON.stringify(orderData, null, 2));
+        const element = document.getElementById('paypal-button-container');
+        element.innerHTML = '';
+        element.innerHTML = '<h3>Thank you for your payment!</h3>';
+        window.setTimeout(() => {
+          location.reload();
+        }, 1000)
+      });
+    },
+    onError: function(err) {
+      console.log(err);
+    }
+  }).render('#paypal-button-container');
+}
+
 function paypalModal() {
   const modalOpen = document.querySelector('#modal-open');
   const modalClose = document.querySelector('#modal-close');
@@ -15,8 +45,25 @@ function vhVariable() {
   document.documentElement.style.setProperty('--vh', `${vh}px`);
 }
 
+function ballLightning() {
+  const xRayCats = document.querySelectorAll('.x-ray-cat');
+  xRayCats.forEach(cat => {
+    cat.addEventListener('mouseover', (event)=> {
+      const {x, y, target} = event;
+      const ballLightning = target.previousElementSibling;
+      // ballLightning.style.left = `${x}px`;
+      // ballLightning.style.top = `${y}px`;
+      // console.log(ballLightning.style)
+
+      console.log(ballLightning);
+    })
+  })
+}
+
 document.addEventListener('DOMContentLoaded', ()=> {
+  initPayPalButton();
   paypalModal();
   vhVariable();
+  ballLightning();
   window.addEventListener('resize', vhVariable)
 })
