2bb33b75746746277ac9a134d1b5e2aba9710eb4
Isaac Chabon <ichabon@me.com>
2021-12-23 16:57:33 -0500

    set up slideshow

--- script.js
+++ script.js
@@ -59,9 +59,34 @@ function vhVariable() {
   document.documentElement.style.setProperty('--vh', `${vh}px`);
 }
 
+function setActiveImage(index){
+  const carouselImgs = document.querySelectorAll('.work .carousel > img');
+  const currentActive = document.querySelector('.work .carousel > img.active');
+  currentActive.classList.remove('active')
+  carouselImgs[index].classList.add('active');
+}
+
 function imgCarousel () {
   const carouselImgs = document.querySelectorAll('.work .carousel > img');
-  console.log(carouselImgs);
+  let leftArrow = document.querySelector('.work .carousel .arrow-left');
+  let rightArrow = document.querySelector('.work .carousel .arrow-right');
+  let index = 0;
+  leftArrow.addEventListener('click', () => {
+    if (index > 0){
+      index--;
+    } else {
+      index = carouselImgs.length - 1;
+    }
+    setActiveImage(index);
+  })
+  rightArrow.addEventListener('click', () => {
+    if (index < carouselImgs.length -1){
+      index++;
+    } else {
+      index = 0;
+    }
+    setActiveImage(index);
+  })
 }
 
 document.addEventListener('DOMContentLoaded', ()=> {
