5764eb1e2fe12f9e1b4e4b72292f81f888a3b355
Isaac Chabon <isaac@sheridan.co>
2022-01-05 09:38:05 -0500

    test adding basic email form

--- script.js
+++ script.js
@@ -1,48 +1,4 @@
-// function initPayPalButton() {
-//   function initPayPalButton() {
-//     paypal.Buttons({
-//       style: {
-//         shape: 'pill',
-//         color: 'gold',
-//         layout: 'vertical',
-//         label: 'paypal',
-        
-//       },
-
-//       createOrder: function(data, actions) {
-//         return actions.order.create({
-//           purchase_units: [{"description":"Infinity Rocks 2022 Calendar","amount":{"currency_code":"USD","value":18.88}}]
-//         });
-//       },
-
-//       onApprove: function(data, actions) {
-//         return actions.order.capture().then(function(orderData) {
-          
-//           // Full available details
-//           console.log('Capture result', orderData, JSON.stringify(orderData, null, 2));
-
-//           // Show a success message within this page, e.g.
-//           const element = document.getElementById('paypal-button-container');
-//           element.innerHTML = '';
-//           element.innerHTML = '<h3>Thank you for your payment!</h3>';
-//           window.setTimeout(() => {
-//             location.reload();
-//           }, 1000)
-
-//           // Or go to another URL:  actions.redirect('thank_you.html');
-          
-//         });
-//       },
-
-//       onError: function(err) {
-//         console.log(err);
-//       }
-//     }).render('#paypal-button-container');
-//   }
-//   initPayPalButton();
-// }
-
-function paypalModal() {
+function modal() {
   const modalOpen = document.querySelector('#modal-open');
   const modalClose = document.querySelector('#modal-close');
 
@@ -92,7 +48,7 @@ function imgCarousel () {
 document.addEventListener('DOMContentLoaded', ()=> {
   document.body.classList.add('loaded');
   // initPayPalButton();
-  paypalModal();
+  modal();
   vhVariable();
   imgCarousel();
   
